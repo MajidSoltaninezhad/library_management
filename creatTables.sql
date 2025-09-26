a83e018bfeeee26e5b0546ba05e56a3884fd6516
MajidSoltaninezhad <nezhad.majid@gmail.com>
2025-09-26 23:50:09 +0800

    Edit tables and fix schema issues

--- creatTables.sql
+++ creatTables.sql
@@ -1,73 +1,95 @@
 -- CREATE DATABASE library;
 
---// Creat tables
-DROP TABLE IF EXISTS branch;
+--// Create tables
+DROP TABLE IF EXISTS branch CASCADE;
 CREATE TABLE branch (
     branch VARCHAR(10) PRIMARY KEY,
     manager_id VARCHAR(10),
-    branch_address VARCHAR(100),
-    conntact_number VARCHAR(15) 
+    branch_address VARCHAR(55),
+    conntact_number VARCHAR(10) 
 );
 
 
-DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS employees CASCADE;
 CREATE TABLE employees (
     emp_id VARCHAR(10) PRIMARY KEY,
-    emp_name VARCHAR(50),
-    position VARCHAR(30),
+    emp_name VARCHAR(25),
+    position VARCHAR(15),
     salary INT, 
-    branch_id VARCHAR(10)
+    branch_id VARCHAR(25)
 );
 
-DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS books CASCADE;
 CREATE TABLE books (
-    isbn VARCHAR(10) PRIMARY KEY,
-    book_title VARCHAR(50),
-    category VARCHAR(50),
+    isbn VARCHAR(20) PRIMARY KEY,
+    book_title VARCHAR(75),
+    category VARCHAR(10),
     rental_price FLOAT,
-    status VARCHAR(20),
-    author VARCHAR(50),
-    publisher VARCHAR(50)
+    status VARCHAR(15),
+    author VARCHAR(35),
+    publisher VARCHAR(55)
 );
 
-DROP TABLE IF EXISTS members;
+DROP TABLE IF EXISTS members CASCADE;
 CREATE TABLE members (
-    member_id VARCHAR PRIMARY KEY,
-    member_name VARCHAR(50),
-    member_address VARCHAR(100),
+    member_id VARCHAR(10) PRIMARY KEY,
+    member_name VARCHAR(25),
+    member_address VARCHAR(75),
     reg_date DATE
 );
 
-DROP TABLE IF EXISTS issued_status;
+DROP TABLE IF EXISTS issued_status CASCADE;
 CREATE TABLE issued_status (
     issued_id VARCHAR(10) PRIMARY KEY,
     issued_member_id VARCHAR(10) ,
-    issued_book_name VARCHAR(50),
+    issued_book_name VARCHAR(75),
     issued_date DATE,
-    issued_book_isbn VARCHAR(20),
-    issued_emp_id VARCHAR(20)
+    issued_book_isbn VARCHAR(25),
+    issued_emp_id VARCHAR(10)
 ); 
 
-DROP TABLE IF EXISTS returned_status;
+DROP TABLE IF EXISTS returned_status CASCADE;
 CREATE TABLE returned_status (
     return_id VARCHAR(10) PRIMARY KEY,
-    return_member_id VARCHAR(10),
-    return_book_name VARCHAR(50),
+    issued_id VARCHAR(10),
+    return_book_name VARCHAR(75),
     return_date DATE,
-    return_book_isbn VARCHAR(20),
-    return_emp_id VARCHAR(10)
+    return_book_isbn VARCHAR(20)
+    
 );  
 
 -- I set wrong type for member_id and i need to cahnge it to varchar(10)
-ALTER TABLE members
-ALTER COLUMN member_id TYPE VARCHAR(10)
-USING member_id::VARCHAR;
+ALTER TABLE books
+ALTER COLUMN isbn TYPE VARCHAR(20)
+USING isbn::VARCHAR;
+
+ALTER TABLE branch
+RENAME COLUMN branch TO branch_id;  
 
 --FOREIGN KEY CONSTRAINTS for datamodel
 
-ALTER TABLE issued_status
-ADD      fk_members
-FOREIGN KEY (issued_member_id) REFERENCES members(member_id);
+    ALTER TABLE issued_status
+    ADD CONTRAINT fk_members
+    FOREIGN KEY (issued_member_id)
+    REFERENCES members(member_id);
+
+    ALTER TABLE issued_status
+    ADD CONSTRAINT fk_books
+    FOREIGN KEY (issued_book_isbn)
+    REFERENCES books(isbn);
+
+    ALTER TABLE issued_status
+    ADD CONSTRAINT fk_employees
+    FOREIGN KEY (issued_emp_id)
+    REFERENCES employees(emp_id);
+
+    ALTER TABLE returned_status
+    ADD CONSTRAINT fk_issued_status
+    FOREIGN KEY (issue d_id)
+
+ 
+
+
 
 
 
